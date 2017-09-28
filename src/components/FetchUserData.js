609a09a5ed7e7e10e170b9c1da852d4eb3b7e3b6
Jesse Tyner-Bryan <jesse.tynerbryan@gmail.com>
2017-09-28 11:15:51 -0400

    cleaned up data-fetching component, getting related artists by random from user's top 5 artists

--- src/components/FetchUserData.js
+++ src/components/FetchUserData.js
@@ -16,36 +16,41 @@ import Loader from './Loader'
 class GetTracks extends React.Component {
 
   componentDidUpdate() {
+    // get user's top tracks and artists
     if (this.props.user.id !== null && this.props.topTracks.length === 0 ) {
       this.props.addTopTracks(this.props.user.id)
       this.props.addTopArtists(this.props.user.id)
-      // this.props.addRecentTracks(this.props.user.id)
-    }
 
+    }
+    // get audio features of top tracks
     if (this.props.topTracks.length > 0 && this.props.topTracksAudioFeatures.length === 0) {
       this.props.addTopTracksAudioFeatures(this.props.user.id, this.props.topTracks)
-      // this.props.addRelatedArtists(this.props.user.id,this.props.topTracks[1].artists[0].id)
     }
 
-    // if (this.props.recentTracks.length > 0 && this.props.recentTracksAudioFeatures.length === 0) {
-    //   this.props.addRecentTracksAudioFeatures(this.props.user.id, this.props.recentTracks)
-    // }
-
+    // get aggregate of top tracks audio features by category
     if (this.props.topTracksAudioFeatures.length > 0 && this.props.aggregateFeaturesOfTopTracks.danceability === 0) {
       this.props.sumFeaturesOfTopTracks(this.props.topTracksAudioFeatures)
     }
 
-    // if (this.props.recentTracksAudioFeatures.length > 0 && this.props.aggregateFeaturesOfRecentTracks.danceability === 0) {
-    //   this.props.sumFeaturesOfRecentTracks(this.props.recentTracksAudioFeatures)
-    // }
+    // get a related artist by random from user's top 5 artists
+    if (this.props.topArtists.length > 0 && this.props.relatedArtists.length === 0) {
+      debugger
+      const topFiveArtists = this.props.topArtists.slice(0, 5)
+      const randomtopFiveArtist = topFiveArtists[Math.floor(Math.random() * topFiveArtists.length)]
+      this.props.addRelatedArtists(this.props.user.id, randomtopFiveArtist.id)
+    }
 
     // if (this.props.aggregateFeaturesOfTopTracks.danceability > 0 && this.props.aggregateFeaturesOfRecentTracks.danceability > 0) {
     //   this.props.history.push('/user-results')
     // }
 
-    if (this.props.topArtists.length > 0 && this.props.relatedArtists.length === 0) {
-      this.props.addRelatedArtists(this.props.user.id, this.props.topArtists[0].id)
-    }
+    // this.props.addRecentTracks(this.props.user.id)
+    // if (this.props.recentTracks.length > 0 && this.props.recentTracksAudioFeatures.length === 0) {
+    //   this.props.addRecentTracksAudioFeatures(this.props.user.id, this.props.recentTracks)
+    // }
+    // if (this.props.recentTracksAudioFeatures.length > 0 && this.props.aggregateFeaturesOfRecentTracks.danceability === 0) {
+    //   this.props.sumFeaturesOfRecentTracks(this.props.recentTracksAudioFeatures)
+    // }
 
   }
 
