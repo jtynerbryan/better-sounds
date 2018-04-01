8615fff465b3289592a93806adcf20dfc8965c9a
Jesse Tyner-Bryan <jesse.tynerbryan@gmail.com>
2018-04-01 12:21:33 -0400

    changes Welcome Component to stateless functional component, still implementing connect

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -4,39 +4,22 @@ import HomeNavBar from './NavBars/HomeNavBar'
 import LoggedInNavBar from './NavBars/LoggedInNavBar'
 import { Container } from 'semantic-ui-react'
 
-class Welcome extends React.Component {
-
-  render() {
-    if (this.props.isLoggedIn) {
-      return (
-        <div className="welcome">
-          <LoggedInNavBar />
-          <Container textAlign='center' className='welcome-header'>
-          <h1 className="home">Better Sounds</h1>
-          <h2>Analyze your Spotify listening habits</h2>
-          <h2>Create playlists of new music tailored to your needs</h2>
-          </Container>
-        </div>
-      )
-    } else {
-      return (
-        <div className="welcome">
-          <HomeNavBar />
-          <Container textAlign='center' className='welcome-header'>
-          <h1 className="home">Better Sounds</h1>
-          <h2>Analyze your Spotify listening habits</h2>
-          <h2>Create playlists of new music tailored to your needs</h2>
-          </Container>
-        </div>
-      )
-    }
-  }
+const Welcome = (props) => {
+  return (
+    <div className="welcome">
+      { props.isLoggedIn ? <LoggedInNavBar /> : <HomeNavBar /> }
+      <Container textAlign='center' className='welcome-header'>
+      <h1 className="home">Better Sounds</h1>
+      <h2>Analyze your Spotify listening habits</h2>
+      <h2>Create playlists of new music tailored to your needs</h2>
+      </Container>
+    </div>
+  )
 }
 
 function mapStateToProps(state) {
   return {
     isLoggedIn: state.auth.isLoggedIn,
-    user: state.auth.user
   }
 }
 
